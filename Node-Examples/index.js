38e94e8f1f6dc9cf57857a9994cd3236977eeb9a
Jay Malde <maldejay77@gmail.com>
2021-10-23 19:34:29 +0530

    Intro to Express

---
+++ Node-Examples/index.js
@@ -0,0 +1,20 @@
+const rectangle = require('./rectangle');
+var rect = require('./rectangle')
+
+function solveRect(l,b){
+    console.log("Solving for rectangle with l "+l+" b "+b);
+    rect(l,b,(err,rectangle)=>{
+        if(err){
+            console.log("ERROR", err.message);
+        }
+        else{
+            console.log("The area of the rectangle of dimensions l "+l+" and b" +b+" is "+rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l "+l+" and b" +b+" is "+rectangle.perimeter());
+        }
+    });
+    console.log("This Statement is after the call");
+}
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
