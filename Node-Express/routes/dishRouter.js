99c09b0ff182a56b6ef7d9fa411ac51ba9bc0ef7
Jay Malde <maldejay77@gmail.com>
2021-10-23 20:16:54 +0530

    Assignment1

--- Node-Express/routes/dishRouter.js
+++ Node-Express/routes/dishRouter.js
@@ -25,4 +25,20 @@ dishRouter.route('/')
     res.end("Delete All the Dishes");
 });
 
+dishRouter.route('/:dishId')
+    .get((req, res) => {
+      res.end(`Will send details of the dish: ${req.params.dishId} to you!`);
+    })
+    .post((req, res) => {
+      res.statusCode = 403;
+      res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
+    })
+    .put((req, res) => {
+      res.write(`Updating the dish: ${req.params.dishId}\n`);
+      res.end(`Will update the dish: ${req.body.name} with details ${req.body.description}!`);
+    })
+    .delete((req, res) => {
+      res.end(`Deleting dish: ${req.params.dishId}`);
+    });
+
 module.exports=dishRouter;
