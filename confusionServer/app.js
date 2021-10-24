b77ccbad646262de9e81be2c6e2742edb05a0be3
Jay Malde <maldejay77@gmail.com>
2021-10-24 19:50:23 +0530

    Mongo Express REST Part 1

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
 
 var app = express();
 
@@ -22,11 +24,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+const url = 'mongodb://localhost:27017/confusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
