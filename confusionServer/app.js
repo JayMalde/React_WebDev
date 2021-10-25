56a9107f7ef124ac98f098ff2af4f5cc7233bac6
Jay Malde <maldejay77@gmail.com>
2021-10-25 22:26:38 +0530

    Express Session Part 2

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -1,99 +1,72 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require("express-session");
-var FileStore = require("session-file-store")(session);
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var leaderRouter = require('./routes/leaderRouter');
-var promoRouter = require('./routes/promoRouter');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 const mongoose = require('mongoose');
-const Dishes = require('./models/dishes');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
+const url = 'mongodb://localhost:27017/dishes';
+const opts = { useNewUrlParser: true };
+
+mongoose.connect(url, opts)
+    .then(() => console.log('Connected correctly to the server'))
+    .catch(err => console.log(err));
+
+let app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));
+
 app.use(session({
-  name:"session-id",
-  secret:'12345-67890-09876-54321',
-  saveUninitialized:false,
-  resave:false,
-  store:new FileStore()
+  name: 'session-id',
+  secret: '12345-67890-09867-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
 }));
-
-function auth(req,res,next){
-  console.log(req.session);
-  if(!req.session.user){
-    var authHeader=req.headers.authorization;
-    if(!authHeader){
-      var err=new Error("You are not authenticated!");
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status=401;
-      return next(err);
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(":");
-    var username = auth[0];
-    var password = auth[1];
-
-    if(username ==='admin' && password==="password"){
-      // res.cookie('user','admin',{signed:true})
-      req.session.user = 'admin';
-      next();
-    }
-    else{
-      var err=new Error("You are not authenticated!");
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status=401;
-      return next(err); 
-    }
-  }
-  else{
-    if(req.session.user==='admin'){
-      next();
-    }
-    else{
-      var err=new Error("You are not authenticated!");
-      err.status=401;
-      return next(err);   
-    }
-  }
-}
-
-app.use(auth);
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-const url = 'mongodb://localhost:27017/confusion';
-const connect = mongoose.connect(url);
-
-connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(function (req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+    next(createError(401, 'You are not authenticated!'));
+  } else {
+    if (req.session.user === 'authenticated') {
+      next();
+    } else {
+      next(createError(403, 'You are not authenticated!'));
+    }
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
